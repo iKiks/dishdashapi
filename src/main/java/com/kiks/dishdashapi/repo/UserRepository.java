fd23e53d2d2c416153f100cd9ee3438d8d8b7359
iKiks <ikiks.lanre@gmail.com>
2026-01-13 22:08:46 +0100

    user repo updated

--- src/main/java/com/kiks/dishdashapi/repo/UserRepository.java
+++ src/main/java/com/kiks/dishdashapi/repo/UserRepository.java
@@ -4,8 +4,13 @@ import com.kiks.dishdashapi.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
-    User findByUsername(String username);
+    User findByFullName(String fullName);
+    User findByEmail(String email);
+
+    User findAllByDateOfBirth(LocalDate dateOfBirth);
 }

ef3b10c5a8c0206183826f28f1532fc44050a14b
iKiks <ikiks.lanre@gmail.com>
2026-01-13 21:26:31 +0100

    first commit

---
+++ src/main/java/com/kiks/dishdashapi/DishdashapiApplication.java
@@ -0,0 +1,13 @@
+package com.kiks.dishdashapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DishdashapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DishdashapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kiks/dishdashapi/DishdashapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiks.dishdashapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DishdashapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

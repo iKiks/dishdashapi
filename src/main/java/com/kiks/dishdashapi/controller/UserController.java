4c2899499e4799ad439318086ea36f559c94b873
iKiks <ikiks.lanre@gmail.com>
2026-01-14 22:13:37 +0100

    user feature updated

--- src/main/java/com/kiks/dishdashapi/controller/UserController.java
+++ src/main/java/com/kiks/dishdashapi/controller/UserController.java
@@ -3,6 +3,9 @@ package com.kiks.dishdashapi.controller;
 import com.kiks.dishdashapi.model.User;
 import com.kiks.dishdashapi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +16,25 @@ public class UserController {
     @Autowired
     private UserService service;
 
+    Autowired
+    AuthenticationManager authenticationManager;
+
     @PostMapping("register")
     public User register(@RequestBody User user) {
         return service.saveUser(user);
+    }
+
+    @PostMapping("login")
+    public String login(@RequestBody User user){
+
+        Authentication authentication = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword()));
+
+        if(authentication.isAuthenticated())
+            return jwtService.generateToken(user.getUsername());
+        else
+            return "Login Failed";
+
     }
 
 }
